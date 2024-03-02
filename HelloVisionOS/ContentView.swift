08e7f9f71007c4597ece3baf847d2a6e2c2eeb1d
Himindu <himindukularathne@gmail.com>
2024-03-02 07:32:06 +0530

    Initial Commit

---
+++ HelloVisionOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HelloVisionOS
+//
+//  Created by Himindu Kularathne on 2024-03-02.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ HelloVisionOS/HelloVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloVisionOSApp.swift
+//  HelloVisionOS
+//
+//  Created by Himindu Kularathne on 2024-03-02.
+//
+
+import SwiftUI
+
+@main
+struct HelloVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
